b1398e4135dc6f42a89fbe2af91f4ef01c6b7a21
MrLapcha <rkhem1514@gmail.com>
2024-02-08 12:47:10 +0545

    updating

--- src/About.js
+++ src/About.js
@@ -1,51 +1,53 @@
-import React from 'react'
+import React from 'react';
+import './Style.css'; // Import your CSS file for styling
 
 const About = () => {
   return (
-    <section id="about">
-    <div className="container mt-4 pt-4">
+    <section id="about" className="about-section">
+      <div className="container mt-4 pt-4">
         <h1 className="text-center">About Me</h1>
         <div className="row mt-4">
-            <div className="col-lg-4">
-                <img src= {require ("./images/DSC_1660.JPG")}  className= "imageAboutPage" alt=""/>
-            </div>
+          <div className="col-lg-4">
+            <img src={require("./images/DSC_1660.JPG")} className="imageAboutPage" alt="" />
+          </div>
 
-            <div className="col-lg-8">
-                <p> The only way to achieve the impossible is to believe it is possible.
-                    
-                </p>
-                <div className="row mt-3">
-                    <div className="col-md-6">
-                        <ul>
-                            <li>Name: Khemraj</li>
-                            <li>Age: 21</li>
-                            <li>Occupation: Developer and Creator</li>
-
-                        </ul>
-                    </div>
-                    <div className="col-md-6">
-                        <ul>
-                            <li>Phone: +977 9815055006</li>
-                            <li>Email: khemrajlapcha14@gmail.com</li>
-                            <li>Address: Nepal</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="row mt-3">
-                    <p>Hello, I'm Khemraj Lapcha, a dedicated student currently pursuing my BSc.
-                         CSIT at Mechi Multiple Campus, currently in the 6th semester.
-                          I reside in Buddhashanti-6, Jhapa. My passion lies in the world of coding,
-                           where I enjoy exploring and creating innovative solutions.
-                            Apart from academics, I find joy in music,
-                             and I have an avid interest in continually learning new things.
-                              This journey of combining my love for coding, exploration of new knowledge,
-                               and appreciation for music keeps me motivated and excited about the possibilities that lie ahead. </p>
-                </div>
+          <div className="col-lg-8">
+            <p className="about-text">
+              The only way to achieve the impossible is to believe it is possible.
+            </p>
+            <div className="row mt-3">
+              <div className="col-md-6">
+                <ul className="about-info-list">
+                  <li>Name: Khemraj</li>
+                  <li>Age: 21</li>
+                  <li>Occupation: Developer and Creator</li>
+                </ul>
+              </div>
+              <div className="col-md-6">
+                <ul className="about-info-list">
+                  <li>Phone: +977 9815055006</li>
+                  <li>Email: khemrajlapcha14@gmail.com</li>
+                  <li>Address: Nepal</li>
+                </ul>
+              </div>
             </div>
+            <div className="row mt-3">
+              <p className="about-description">
+                Hello, I'm Khemraj Lapcha, a dedicated student currently pursuing my BSc.
+                CSIT at Mechi Multiple Campus, currently in the 6th semester.
+                I reside in Buddhashanti-6, Jhapa. My passion lies in the world of coding,
+                where I enjoy exploring and creating innovative solutions.
+                Apart from academics, I find joy in music,
+                and I have an avid interest in continually learning new things.
+                This journey of combining my love for coding, exploration of new knowledge,
+                and appreciation for music keeps me motivated and excited about the possibilities that lie ahead.
+              </p>
+            </div>
+          </div>
         </div>
-    </div>
-</section>
-  )
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './Style.css'; // Import your CSS file for styling
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg fixed-top navbar-dark navbarScroll">
-    <div className="container">
+      <div className="container">
         <a className="navbar-brand" href="#">Khemraj Lapcha</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#home">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#about">About</a>
-                </li>
-            </ul>
-            
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
+            </li>
+          </ul>
         </div>
-    </div>
-</nav>
-  )
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
